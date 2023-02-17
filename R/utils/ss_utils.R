0a57d22cb9817bf71b9a425bb2e7f9af37351ebc
Joshua Zahner <joshuazahner@gmail.com>
2023-02-17 08:00:07 -0800

    Simplified ss_executables directory

---
+++ R/utils/ss_utils.R
@@ -0,0 +1,52 @@
+library(stringr)
+
+get.ss.exe.path <- function(os, ss_version="3.30.21", fname.extra=""){
+  
+  if(!(os %in% c("win32", "win64", "win", "osx", "linux"))){
+    stop(
+      "Unknown OS provided. Must be one of: 'win', 'win32', 'win64', 'osx', 'linux'.
+       Note that newer version of SS do not support win32.
+      "
+    )
+  }
+  
+  if(!(ss_version %in% c("3.24.U", "3.30.21"))){
+    stop(
+      "Unsupported SS version. Please used either 3.24.U or 3.30.21."
+    )
+  }
+  
+  file.extension = ""
+  if(os %in% c("win32", "win64", "win")){
+    file.extension <- ".exe"
+  }
+  
+  version.components <- stringr::str_split(ss_version, "[.]")[[1]]
+  major.version <- version.components[1]
+  minor.version <- version.components[2]
+  patch.number  <- version.components[3]
+  
+  if(as.numeric(minor.version) >= 30 & os %in% c("win32", "win64", "win")){
+    os <- "win"
+  }
+  
+  exe.dir <- file.path(here::here(), "model", "ss_executables")
+  
+  version.dir <- paste0("SS_V", major.version, "_", minor.version, "_", patch.number)
+  
+  if(nchar(fname.extra) > 0){
+    fname.extra <- paste0("_", fname.extra)
+  } 
+  
+  ss.file <- paste0("ss_", os, fname.extra, file.extension)
+  
+  exe.path <- file.path(exe.dir, version.dir, ss.file)
+  if(!file.exists(exe.path)){
+    warning("Note that this executable does not exist.")
+  }
+  
+  return(exe.path)
+  
+}
+
+
