f8c5f30792e299b93f28b400d792cea5d6c613a9
Jane Sullivan <jane.sullivan@noaa.gov>
2023-03-06 15:59:15 -0900

    progress from group work session. errors still occurring in the output length comps. do not use!!

--- R/landings/SST_PacFIN_expansion.R
+++ R/landings/SST_PacFIN_expansion.R
@@ -2,6 +2,7 @@
 # Contact: Haley Oleynik
 # Last updated March 2023
 
+# set up ----
 libs <- c('tidyverse', 'patchwork', 'ggthemes', 'ggridges')
 if(length(libs[which(libs %in% rownames(installed.packages()) == FALSE )]) > 0) {
   install.packages(libs[which(libs %in% rownames(installed.packages()) == FALSE)])}
@@ -11,7 +12,16 @@ lapply(libs, library, character.only = TRUE)
 # pak::pkg_install('pfmc-assessments/nwfscSurvey',upgrade = TRUE)
 library(PacFIN.Utilities)
 library(nwfscSurvey)
-<<<<<<< HEAD
+
+# take black out of colorblind theme
+scale_fill_colorblind7 = function(.ColorList = 2L:8L, ...){
+  scale_fill_discrete(..., type = colorblind_pal()(8)[.ColorList])
+}
+
+# Color
+scale_color_colorblind7 = function(.ColorList = 2L:8L, ...){
+  scale_color_discrete(..., type = colorblind_pal()(8)[.ColorList])
+}
 
 # we're not using any length comps from unidentified thornyheads because
 # longspine and shortspine grow differently.
@@ -19,21 +29,11 @@ library(nwfscSurvey)
 #	PacFIN Data Expansion for Shortspine thornyhead 2023 -----
 
 # Pull PacFIN biological (bds) data 
-bds_file = "data/raw/PacFIN.SSPN.bds.17.Jan.2023.RData" # not hosted on github -- confidential
+bds_file_short = "PacFIN.SSPN.bds.17.Jan.2023"
+bds_file = paste0("data/raw/", bds_file_short, ".RData") # not hosted on github -- confidential
 load(bds_file)
-=======
-library(reshape2)
-library(ggthemes)
-library(ggridges)
-library(cowplot)
-library(ggthemes)
-###############################################################################
-#	PacFIN Data Expansion for Shortspine thornyhead 2023 ----------------------------------------
-
-# Pull PacFIN biological (bds) data 
-load("data/raw/PacFIN.SSPN.bds.17.Jan.2023.RData")
->>>>>>> fb7e37e1f8286c3644b75ef714eeacda4bc19f70
 out = bds.pacfin 
+rm(bds.pacfin)
 
 # Clean data 
 Pdata <- cleanPacFIN(
@@ -42,72 +42,59 @@ Pdata <- cleanPacFIN(
   CLEAN = TRUE, 
   verbose = TRUE)
 
-Pdata2 <- Pdata
-
 # Plot fishery length comps 
-Pdata2$year <- as.character(Pdata2$year) # make year a character 
+Pdata$year <- as.character(Pdata$year) # make year a character 
 
-ggplot(Pdata2 %>% 
-               filter((between(lengthcm, 6, 80))), aes(x=lengthcm,y=year, fill = state, color = state)) + 
+ggplot(Pdata %>% 
+               filter((between(lengthcm, 6, 80))), 
+       aes(x=lengthcm,y=year, fill = state, color = state)) + 
   geom_density_ridges(alpha = 0.5) + 
   facet_wrap(vars(SEX)) + 
   ylab("") + 
   xlab("Length (cm)") +
-  ggtitle("Shortspine thornyhead fishery length compositions") + 
-  scale_fill_colorblind() +
-  scale_color_colorblind() +
+  labs(fill = 'State', col = 'State') +
+  ggtitle("Shortspine Thornyhead Fishery Length Compositions") + 
+  scale_fill_colorblind7() +
+  scale_color_colorblind7() +
   theme_classic()
 
-#ggsave("outputs/fishery data/SST_PacFIN_fishery_lencomps.png", dpi=300, height=7, width=10, units='in')
+ggsave("outputs/fishery_data/SST_PacFIN_fishery_lencomps.png", dpi=300, height=7, width=10, units='in')
 
 # create fleet structure for expansion
 # Check fleet structure
 table(Pdata$geargroup)
 
-# Create fleet structure in Pdata
-Pdata["geargroup"][Pdata["geargroup"] == "HKL"] <- "NONTWL"
-Pdata["geargroup"][Pdata["geargroup"] == "POT"] <- "NONTWL"
-Pdata["geargroup"][Pdata["geargroup"] == "NET"] <- "NONTWL"
-table(Pdata$geargroup)
-
-# Assign fleet - STATE_GEAR
-Pdata$fleet[Pdata$state == "CA"] = paste0("CA", "_", Pdata$geargroup[Pdata$state == "CA"])
-Pdata$fleet[Pdata$state == "OR"] = paste0("OR", "_", Pdata$geargroup[Pdata$state == "OR"])
-Pdata$fleet[Pdata$state == "WA"] = paste0("WA", "_", Pdata$geargroup[Pdata$state == "WA"])
-table(Pdata$fleet)
+# Create fleet structure in Pdata: the stratification for
+# expansion, and calculate expansions
+Pdata <- Pdata %>% 
+  dplyr::mutate(geargroup = ifelse(geargroup == 'TWL', 'TWL', 'NONTWL'),
+                fleet = paste0(state, "_", geargroup))
 
 # Load in sex specific growth estimates from survey data 
-#LWsurvey <- read.csv("outputs/length-weight/lw_parameters_NWFSCcombo.csv")
-fa = 6.550678e-06 # a param female 
-# fa <- LWsurvey$alpha[LWsurvey$sex=="Female"]
-fb = 3.179796 # b param female
-# fb <- LWsurvey$beta[LWsurvey$sex=="Female"]
-ma = 6.657707e-06
-# ma <- LWsurvey$alpha[LWsurvey$sex=="Male"]
-mb = 3.172393
-# mb <- LWsurvey$beta[LWsurvey$sex=="Male"]
-ua = (fa + ma) / 2
-## ua <- LWsurvey$alpha[LWsurvey$sex=="Sexes_combined"]
-ub = (fb + mb) / 2
-## ub <- LWsurvey$beta[LWsurvey$sex=="Sexes_combined"]
+LWsurvey <- read.csv("outputs/length-weight/lw_parameters_NWFSCcombo.csv")
+fa <- LWsurvey$alpha[LWsurvey$sex=="Female"]
+fb <- LWsurvey$beta[LWsurvey$sex=="Female"]
+ma <- LWsurvey$alpha[LWsurvey$sex=="Male"]
+mb <- LWsurvey$beta[LWsurvey$sex=="Male"]
+ua = LWsurvey$alpha[LWsurvey$sex=="Sexes_combined"]
+ub = LWsurvey$beta[LWsurvey$sex=="Sexes_combined"]
 
-################################################################################
-# Data checking -------------------------------------------------------
+# Data checking -----
 # Check lengths 
 quantile(Pdata$lengthcm, na.rm = TRUE) 
 summary(Pdata$lengthcm)
 table(Pdata$geargroup[is.na(Pdata$length)])
 table(Pdata$fleet[is.na(Pdata$length)])
-# Length data is mostly missing for CA NonTrawl. Only a small percentage of the dataset
+# Length data is mostly missing for CA NonTrawl. Only a small percentage of the
+# dataset
+
+# in early iterations of the analysis, we identified 3 fish from 1981 caught in
+# in California that are skewing the expansion (sample id = 1981223128134).
+# Filter out these 3 weird fish (tiny proportion of fish sampled).
+Pdata <- Pdata %>% filter(AGENCY_SAMPLE_NUMBER != 1981223128134)
 
 # W-L plot 
 ggplot(Pdata, aes(x = lengthcm, y = weightkg)) +
-  geom_jitter() + 
-  geom_point(aes(col = SEX), size = 2) +
-  scale_colour_viridis_d()
-
-# try with length
-ggplot(Pdata, aes(x = length, y = weightkg)) +
   geom_jitter() + 
   geom_point(aes(col = SEX), size = 2) +
   scale_colour_viridis_d()
@@ -123,83 +110,85 @@ filter(Pdata, weightkg < 0.15) %>%
 # what are this odd data points?
 Pdata %>%
   filter(lengthcm > 39 & weightkg < 0.05) %>% 
-  dplyr::select(year) %>%
+  dplyr::select(year, fleet) %>%
   table()
 # --> Mainly recent data (2020) from Washington state
 
-# Look at small weights  
+# Fix the weight errors using the conversion between lb and kg
 Pdata %>%
-  mutate(weightkg=ifelse(lengthcm > 39 & weightkg < 0.05, weightkg*0.453592*1000, weightkg)) %>% 
-  select(lengthcm, weightkg, SEX) %>%
+  dplyr::mutate(weightkg=ifelse(lengthcm > 39 & weightkg < 0.05, 
+                                weightkg*0.453592*1000, weightkg)) %>% 
+  dplyr::select(lengthcm, weightkg, SEX) %>%
   ggplot(aes(x = lengthcm, y = weightkg)) +
   geom_jitter() + 
   geom_point(aes(col = SEX), size = 2) +
   scale_colour_viridis_d()
 
-# 3 fish from 1981 caught in California are skewing the expansion (sample id = 1981223128134). Filter out these 3 weird fish (tiny proportion of fish sampled).
-test <- Pdata %>% filter(AGENCY_SAMPLE_NUMBER != 1981223128134)
+Pdata <- Pdata %>%
+  dplyr::mutate(weightkg=ifelse(lengthcm > 39 & weightkg < 0.05, 
+                                weightkg*0.453592*1000, weightkg)) 
 
-# filter out small weights? 
-#test <- test %>% filter(weightkg > 0.025 | is.na(weightkg))
+# First stage expansion -----
 
-# or, alternatively, consider the magical conversion
-test <- test %>%
-  mutate(weightkg=ifelse(lengthcm > 39 & weightkg < 0.05, weightkg*0.453592*1000, weightkg)) 
-
-# check W-L again 
-ggplot(test, aes(x = lengthcm, y = weightkg)) +
-  geom_jitter() + 
-  geom_point(aes(col = SEX), size = 2) +
-  scale_colour_viridis_d()
-
-#####################################################################
-# Specify fleets, the stratification for expansion, and calculate expansions
-
-# First stage expansion ----------NOTE: TEST---------------------------------
 # expand comps to the trip level
 Pdata_exp <- getExpansion_1(
-  Pdata = test,
-  plot = file.path("outputs/fishery data"),
+  Pdata = Pdata,
+  plot = file.path("outputs/fishery_data"),
   fa = fa, fb = fb, ma = ma, mb = mb, ua = ua, ub = ub) # weight-length params
 
 # check expansion factors (want them to be < 500)
 hist(Pdata_exp$Expansion_Factor_1_L)
 
 # check the filled weight values are consistent with observations
+# dev.off() # use if plot isn't rendering
 Pdata_exp %>%
-  mutate(is.weight=ifelse(is.na(weightkg), "N", "Y")) %>%
+  dplyr::mutate(is.weight=ifelse(is.na(weightkg), "N", "Y")) %>%
   ggplot(aes(x = lengthcm, y = bestweight)) +
   geom_jitter() + 
   geom_point(aes(col = is.weight), size = 2) +
   scale_colour_viridis_d() +
   theme_bw()
 
-# Second stage expansion -----------NOTE: TEST--------------------------------
+# Second stage expansion ----
 # expand comps up to the state and fleet (gear group)
 
-# Read in processed PacFIN catch data 
-## Each state should be kept seperate for state based expansions for each gear fleet within that state. Landings reported data from pacfin - separate for each state because different sampling and coverage 
-catch <- read.csv("data/processed/SST_PacFIN_landings_2023.csv")
+# Read in processed PacFIN catch data - Each state should be kept seperate for
+# state based expansions for each gear fleet within that state. Landings
+# reported data from pacfin - separate for each state because different sampling
+# and coverage
+catch <- read.csv("data/processed/SST_PacFIN_length-comp-landings_2023.csv")
 
 # second stage expansion 
-## # The stratification.col input below needs to be the same as in the catch csv file
+# The stratification.col input below needs to be the same as in the catch csv file
 Pdata_exp <- getExpansion_2(
   Pdata = Pdata_exp, 
   Catch = catch, # catch file - needs to match state and fleet 
   Units = "MT", 
   stratification.cols = c("state", "geargroup"),
-  savedir = file.path("outputs/fishery data"))
+  savedir = file.path("outputs/fishery_data"))
 
-# Calculate the final expansion size ------------------------------------
-# look expansion factors and caps (26 is a tight range)
+# look expansion factors and caps ???
 # multiply expansion factors together 
 # cap values applies 0.95 quantile (anything outside gets capped)
 Pdata_exp$Final_Sample_Size <- capValues(Pdata_exp$Expansion_Factor_1_L * Pdata_exp$Expansion_Factor_2)
 
+# Calculate the final expansion size -----
+
+# redefine strata definitions to north/south instead by state
+# Trawl_n=1
+# Trawl_s=2
+# Non-trawl_N=3
+# Non-trawl_S=4
+Pdata_exp <- Pdata_exp %>% 
+  dplyr::mutate(fleet = case_when(fleet %in% c('OR_TWL', 'WA_TWL') ~ 1,
+                                  fleet %in% c('CA_TWL') ~ 2,
+                                  fleet %in% c('OR_NONTWL', 'WA_NONTWL') ~ 3,
+                                  fleet %in% c('CA_NONTWL') ~ 4))
+
 # data frame with expansion factors - only records with lengths, take out na
 # dataframe with composition by length bin, state, year, gear
 length_comps <- getComps(
-  Pdata = Pdata_exp[!is.na(Pdata_exp$lengthcm), ], 
+  Pdata = Pdata_exp2[!is.na(Pdata_exp2$lengthcm), ], 
   Comps = "LEN")
 
 # diagnostics 
@@ -211,11 +200,10 @@ ggplot(Pdata, aes(x = lengthcm, y = count, fill = SEX))  +
   geom_histogram(aes(y = count), position="stack", stat="identity") +
   scale_fill_viridis_d()
 
-#################################################################################
 # Create the length composition data
 # code to do sex ratio: 
-# Commenting out for now because I don't want to assign unsexed 
-# due to the dimorphic growth
+# Commenting out for now because I don't want to assign unsexed due to the
+# dimorphic growth
 # There area a fair number of U in CA and in the early years of WA
 # length_compSR <- doSexRatio(
 #	CompData = length_comps, 
@@ -225,39 +213,36 @@ ggplot(Pdata, aes(x = lengthcm, y = count, fill = SEX))  +
 
 # length bins 
 len_bins = c(seq(6, 72, 2))
-out_name = sub(pattern = "(.*)\\..*$", replacement = "\\1", bds_file)
 
 writeComps(
   inComps = length_comps, 
-  fname = file.path("outputs/fishery data", paste0("Lengths_", out_name, ".csv")), # tell file name for .csv
+  fname = file.path("outputs/fishery_data", paste0("length_comps/", bds_file_short, ".csv")), # tell file name for .csv
   lbins = len_bins, 
   sum1 = TRUE, # sum comps to 1, SS does this for you 
   partition = 2, # only retained fish 
   digits = 4, # round 
   dummybins = FALSE)
 
-###############################################################################
-# Let's format the csv files for direct use in SS3
-###############################################################################
 
+# format the csv files for SS3
 out = read.csv(
-  file.path(dir, "pacfin_bds", "forSS", paste0("Lengths_", out_name, ".csv")), 
+  file.path("outputs/fishery_data", paste0("length_comps/", bds_file_short, ".csv")), 
   skip = 3, 
   header = TRUE)
 
 start = 1 
 end   = which(as.character(out[,1]) %in% c(" Females only ")) - 1 
 cut_out = out[start:end,]
-
-# format the length comps
-cut_out$fleet[cut_out$fleet =="CA_ALL"] = 1
-cut_out$fleet[cut_out$fleet =="WA_OR_ALL"] = 2
+# 
+# # format the length comps
+# cut_out$fleet[cut_out$fleet =="CA_ALL"] = 1
+# cut_out$fleet[cut_out$fleet =="WA_OR_ALL"] = 2
 
 ind = which(colnames(cut_out) %in% paste0("F", min(len_bins))):
   which(colnames(cut_out) %in% paste0("M", max(len_bins)))
 format = cbind(cut_out$year, cut_out$month, cut_out$fleet, cut_out$sex, cut_out$partition, cut_out$InputN, cut_out[,ind])
 colnames(format) = c("year", "month", "fleet", "sex", "part", "InputN", colnames(cut_out[ind]))
-format = format[format$year != 2021, ]
+# format = format[format$year != 2021, ]
 write.csv(
   format, 
   file = file.path("outputs/fishery data", paste0("Lcomps_for_SS3_", out_name, ".csv")), 
