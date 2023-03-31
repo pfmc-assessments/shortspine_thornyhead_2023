7f021102f7093695a0bd601398eb24ded855ef82
AndreaNOdell <anodell@ucdavis.edu>
2023-03-31 13:06:42 +1000

    Corrected the calculation of frequency of occurrence in tows

--- R/survey/SST_surveys_2023.R
+++ R/survey/SST_surveys_2023.R
@@ -226,9 +226,9 @@ nwfsc.combo.length.freq <- SurveyLFs.fn(dir      = out.dir,
 
 NWFSC.Combo.master <- PullCatch.fn(SurveyName = "NWFSC.Combo")
 
-NWFSC.Combo.by.tow = NWFSC.Combo.master[NWFSC.Combo.master$Depth_m <= 500,] %>% 
+NWFSC.Combo.by.tow = NWFSC.Combo.master[NWFSC.Combo.master$Depth_m >= 500,] %>% 
   group_by(Tow) %>% 
-  summarise(n = n(), sst = length(which(Common_name == "shortspine thornyhead"))) %>% 
+  summarise(n = n(), sst = length(which(Common_name == "shortspine thornyhead" & Subsample_count >= 1))) %>% 
   mutate(sst_present = sst >= 1)
 
 SST.freq.occurrence.tows = mean(NWFSC.Combo.by.tow$sst_present) 
