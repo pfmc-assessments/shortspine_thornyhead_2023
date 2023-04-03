e402eac81a30ddbcfd1c442191d63f3e8b4784d6
Jane Sullivan <jane.sullivan@noaa.gov>
2023-04-03 10:15:22 -0800

    more fun maturity updates!

--- R/bio/fit_ogive_length_mat_with_env.R
+++ R/bio/fit_ogive_length_mat_with_env.R
@@ -22,14 +22,14 @@ tab_mat <- read.csv("data/raw/SST_maturitydata_forassessment03152023.csv")
 names(tab_mat)
 
 #
-# Same combo survey strata definitions: two southern strata below 34.5º N, one covering 183–550 m and the
-# other covering 550–1280 m. Two central strata between 34.5º N and 40.5º N, had
-# the same depth ranges. North of 40.5º N, three strata were used, covering the
-# ranges 100–183 m, 183–550 m and the other covering 550–1280 m. The depth
-# breaks at 183 m and 550 m are associated with changes in sampling intensity of
-# the survey and are recommended to be used. South of 40.5º N, there are very
-# few shortspine thornyheads shallower than 183 m so no shallow stratum was used
-# in these latitudes.
+# Same combo survey strata definitions: two southern strata below 34.5º N, one
+# covering 183–550 m and the other covering 550–1280 m. Two central strata
+# between 34.5º N and 40.5º N, had the same depth ranges. North of 40.5º N,
+# three strata were used, covering the ranges 100–183 m, 183–550 m and the other
+# covering 550–1280 m. The depth breaks at 183 m and 550 m are associated with
+# changes in sampling intensity of the survey and are recommended to be used.
+# South of 40.5º N, there are very few shortspine thornyheads shallower than 183
+# m so no shallow stratum was used in these latitudes.
 
 tab_mat <- tab_mat %>% 
   filter(!is.na(Latitude) & !is.na(Depth)) %>% 
@@ -39,19 +39,20 @@ tab_mat <- tab_mat %>%
                             between(Latitude, 34.5, 40.5) & Depth >= 550 ~ '4_Central_deep',
                             Latitude > 40.5 & Depth < 183 ~ '5_North_shallow',
                             Latitude > 40.5 & between(Depth, 183, 550) ~ '6_North_mid',
-                            Latitude > 40.5 & Depth > 550 ~ '7_North_deep')) #%>% 
-  # mutate(Strata = factor(Strata, levels = c('South_shallow', 'South_deep',
-  #                                            'Central_shallow', 'Central_deep',
-  #                                            'North_shallow', 'Noth_mid', 'North_deep'),
-         # ordered = TRUE))
+                            Latitude > 40.5 & Depth > 550 ~ '7_North_deep')) 
 nrow(tab_mat)
 table(tab_mat$Strata)
+
+###---------------------------------------###
+###---------- Stratum method ----------------
+###---------------------------------------###
 
 dat_pred2 = crossing(Strata = unique(tab_mat$Strata),
                      Length_cm = 0, #placeholder 
                      Functional_maturity = 1 #placeholder
 )
 
+# Logistic model with separate intercepts for each stratum
 l_mat_strat <- glm(Functional_maturity ~ 1 + Length_cm + Strata,
                   data = tab_mat,
                   family=binomial(link = "logit"))
@@ -67,14 +68,7 @@ combo_catch <- PullCatch.fn(Name = 'shortspine thornyhead',
                             Dir = combo_path,
                             SaveFile = TRUE)
 
-# 7 strata; two southern strata below 34.5º N, one covering 183–550 m and the
-# other covering 550–1280 m. Two central strata between 34.5º N and 40.5º N, had
-# the same depth ranges. North of 40.5º N, three strata were used, covering the
-# ranges 100–183 m, 183–550 m and the other covering 550–1280 m. The depth
-# breaks at 183 m and 550 m are associated with changes in sampling intensity of
-# the survey and are recommended to be used. South of 40.5º N, there are very
-# few shortspine thornyheads shallower than 183 m so no shallow stratum was used
-# in these latitudes.
+# same 7 strata; see definitions above
 combo_strata <- CreateStrataDF.fn(names = c('1_South_shallow', '2_South_deep', 
                                             '3_Central_shallow', '4_Central_deep', 
                                             '5_North_shallow', '6_North_mid', '7_North_deep'), 
@@ -111,7 +105,7 @@ L50_strat = - L50_numerator / L50_denominator
 dat_pred2$L50_strat <- c(L50_strat)
 
 len = 40 # doesn't matter what length you use here
-(kmat <- round(((L50_numerator[,1] + strat_coef[2]*len) / (len - L50_strat[,1])), 2))[1]
+kmat <- ((L50_numerator[,1] + strat_coef[2]*len) / (len - L50_strat[,1]))[1]
 
 out <- dat_pred2 %>% 
   left_join(weights) %>% 
@@ -119,7 +113,7 @@ out <- dat_pred2 %>%
   mutate(kmat = kmat) %>% 
   select(-Length_cm, -Functional_maturity)
 
-out %>% write_csv('outputs/maturity/mhead_weighted_parameters.csv')
+out %>% write_csv('outputs/maturity/head2023_strata_parameters.csv')
   
 dat_pred3 = crossing(Strata = unique(tab_mat$Strata),
                      Length_cm = seq(6, 72, 0.2)
@@ -137,10 +131,6 @@ pmat <- tab_mat %>%
 # Pearson and Gunderson values
 a <- 41.913	
 b <- -2.3046
-# l50 <- 18.19
-
-matatlength <- data.frame(length = lens) %>% 
-  mutate(pmat = )
 
 pred <- data.frame(Length_cm = seq(6, 72, 0.2)) %>% 
   mutate(pred =  1 / (1 + exp(-kmat * (Length_cm - unique(out$L50)))),
@@ -150,9 +140,9 @@ pred <- data.frame(Length_cm = seq(6, 72, 0.2)) %>%
                       version = 'Pearson and Gunderson (2003)'))
 
 ggplot() +
-  geom_line(data = pred, aes(Length_cm, pred, lty = version), size = 1.5) +
+  geom_line(data = pred, aes(Length_cm, pred, lty = version), size = 1.2) +
   geom_point(data = pmat, aes(Length_cm, p_mature, col = Strata)) +
-  geom_line(data = dat_pred3, aes(Length_cm, pred, col = Strata)) +
+  geom_line(data = dat_pred3, aes(Length_cm, pred, col = Strata), size = 0.8) +
   scale_color_colorblind7()+
   scale_fill_colorblind7()+
   scale_y_continuous(labels = scales::comma)+
@@ -165,7 +155,7 @@ ggplot() +
     axis.text = element_text(size=12),
     axis.title = element_text(size=12)
   )
-ggsave(file.path(here::here(), "outputs", "maturity", "mhead_maturity.png"), dpi=300, width=10, height=7, units = "in")
+ggsave(file.path(here::here(), "outputs", "maturity", "head2023_maturity_strata.png"), dpi=300, width=10, height=7, units = "in")
 
 
 ###---------------------------------------###
@@ -262,13 +252,16 @@ tab_mat %>%
   theme_bw()+
   theme(panel.grid = element_blank())
 
-dat_pred_continuous %>%
-  ggplot() +
-  geom_tile(aes(x=Depth, y=Latitude, fill = L50_quad)) +
-  scale_x_reverse() +
-  scale_fill_viridis_c() +
-  labs(fill="L50 (cm)") +
-  theme_bw()
+ggsave(file.path(here::here(), "outputs", "maturity", "head2023_maturity_latdepth_detailed1.png"), dpi=300, width=10, height=7, units = "in")
+
+
+# dat_pred %>%
+#   ggplot() +
+#   geom_tile(aes(x=Depth, y=Latitude, fill = L50_quad)) +
+#   scale_x_reverse() +
+#   scale_fill_viridis_c() +
+#   labs(fill="L50 (cm)") +
+#   theme_bw()
 
 
 ###------------------------------------------###
@@ -290,7 +283,8 @@ catch <- PullCatch.fn(Name = species,
 
 catch %>%
   mutate(tot_cpue = sum(cpue_kg_km2, na.rm=T)) %>%
-  mutate(Depth_m_w = Depth_m * cpue_kg_km2 / tot_cpue, Latitude_dd_w = Latitude_dd * cpue_kg_km2 / tot_cpue) %>%
+  mutate(Depth_m_w = (Depth_m * cpue_kg_km2) / tot_cpue, 
+         Latitude_dd_w = (Latitude_dd * cpue_kg_km2) / tot_cpue) %>%
   summarize(mean_depth = sum(Depth_m_w, na.rm=T),
             mean_lat = sum(Latitude_dd_w, na.rm=T)) %>%
   mutate('(Intercept)'=1,
@@ -311,12 +305,16 @@ L50_denominator_mean = quad_coef_mean[["Length_cm"]]
 L50_quad_mean = - L50_numerator_mean / L50_denominator_mean
 
 len = 40 # doesn't matter what length you use here
-kmat_mean <- round(((L50_numerator[,1] + quad_coef[2]*len) / (len - L50_quad[,1])), 2)[1]
+kmat_mean <- ((L50_numerator[,1] + quad_coef[2]*len) / (len - L50_quad[,1]))[1]
 
 # Pearson and Gunderson values
 a <- 41.913	
 b <- -2.3046
 # l50 <- 18.19
+
+pg_l50 <- -a/b
+pg_matk <- (a + b*len)/(len-pg_l50)
+
 
 pmat <- tab_mat %>% 
   filter(!is.na(Functional_maturity)) %>% 
@@ -325,7 +323,7 @@ pmat <- tab_mat %>%
 
 pred <- data.frame(Length_cm = seq(6, 72, 0.2)) %>% 
   mutate(pred =  1 / (1 + exp(-kmat_mean * (Length_cm - unique(L50_quad_mean)))),
-         version = 'Head 2023 (weighted by abundance)') %>% 
+         version = 'Head 2023 (weighted by CPUE)') %>% 
   bind_rows( data.frame(Length_cm = seq(6, 72, 0.2)) %>% 
                mutate(pred =  1 / (1 + exp(a + b * Length_cm)),
                       version = 'Pearson and Gunderson (2003)'))
@@ -334,7 +332,7 @@ dat_pred %>%
   dplyr::select(-Length_cm) %>%
   crossing(Length_cm = seq(6, 72, 0.2)) %>%
   mutate(pred =  1 / (1 + exp(-kmat_mean * (Length_cm - L50_quad))),
-         version = 'Head 2023 (weighted by abundance)') %>%
+         version = 'Head 2023 (weighted by CPUE)') %>%
   filter(lat_class==unique(dat_pred$lat_class)[which.min(abs(unique(dat_pred$lat_class)-mean(tab_mat$Latitude, na.rm=T)))]) %>%
   mutate(grad="Depth") -> dat_pred_depth_class
   
@@ -342,20 +340,20 @@ dat_pred %>%
   dplyr::select(-Length_cm) %>%
   crossing(Length_cm = seq(6, 72, 0.2)) %>%
   mutate(pred =  1 / (1 + exp(-kmat_mean * (Length_cm - L50_quad))),
-         version = 'Head 2023 (weighted by abundance)') %>%
+         version = 'Head 2023 (weighted by CPUE)') %>%
   filter(depth_class==unique(dat_pred$depth_class)[which.min(abs(unique(dat_pred$depth_class)-mean(tab_mat$Depth, na.rm=T)))]) %>%
   mutate(grad="Latitude") -> dat_pred_lat_class
 
 
 ggplot() +
-  geom_line(data = pred, aes(Length_cm, pred, lty = version), size = 1.5) +
+  geom_line(data = pred, aes(Length_cm, pred, lty = version), size = 1.2) +
   geom_point(data = pmat, aes(Length_cm, p_mature, col = factor(depth_class))) +
-  geom_line(data = dat_pred_depth_class, aes(Length_cm, pred, col = factor(depth_class)), size=1.2) +
+  geom_line(data = dat_pred_depth_class, aes(Length_cm, pred, col = factor(depth_class)), size=0.8) +
   facet_wrap(~grad) +
   scale_color_colorblind7()+
   scale_fill_colorblind7()+
   scale_y_continuous(labels = scales::comma)+
-  labs(x="Year", y="P(mature)", color="Strata", lty = 'Version')+
+  labs(x="Year", y="P(mature)", color="Depth", lty = 'Version')+
   theme_minimal()+
   theme(
     panel.grid.minor = element_blank(),
@@ -364,16 +362,17 @@ ggplot() +
     axis.text = element_text(size=12),
     axis.title = element_text(size=12)
   )
+ggsave(file.path(here::here(), "outputs", "maturity", "head2023_maturity_latdepth_bydepth.png"), dpi=300, width=10, height=7, units = "in")
 
 ggplot() +
-  geom_line(data = pred, aes(Length_cm, pred, lty = version), size = 1.5) +
+  geom_line(data = pred, aes(Length_cm, pred, lty = version), size = 1.2) +
   geom_point(data = pmat, aes(Length_cm, p_mature, col = factor(lat_class))) +
-  geom_line(data = dat_pred_lat_class, aes(Length_cm, pred, col = factor(lat_class)), size=1.2) +
+  geom_line(data = dat_pred_lat_class, aes(Length_cm, pred, col = factor(lat_class)), size=0.8) +
   facet_wrap(~grad) +
   scale_color_colorblind7()+
   scale_fill_colorblind7()+
   scale_y_continuous(labels = scales::comma)+
-  labs(x="Year", y="P(mature)", color="Strata", lty = 'Version')+
+  labs(x="Year", y="P(mature)", color="Latitude", lty = 'Version')+
   theme_minimal()+
   theme(
     panel.grid.minor = element_blank(),
@@ -382,3 +381,19 @@ ggplot() +
     axis.text = element_text(size=12),
     axis.title = element_text(size=12)
   )
+ggsave(file.path(here::here(), "outputs", "maturity", "head2023_maturity_latdepth_bylat.png"), dpi=300, width=10, height=7, units = "in")
+
+
+# Finalize ----
+
+# The preferred option is l_mat_quad but l_mat_strat is likely most
+# representative of the population.
+AIC(l_mat_strat, l_mat_quad)
+
+data.frame(Method = c('Pearson_and_Gunderson_2003', 'Head_2023_latitude_depth', 'Head_2023_strata'),
+           Mat_L50_Fem = c(pg_l50, L50_quad_mean, unique(out$L50)),
+           Mat_slope_Fem = c(pg_matk, kmat_mean, unique(out$kmat))) %>% 
+  write_csv('data/for_ss/maturity_alternatives_2023.csv')
+
+
+
