a46ec29d2fe65ff4efff7989743f20304deec0f2
Jane Sullivan <jane.sullivan@noaa.gov>
2023-04-04 13:55:33 -0800

    Rdata file!

--- R/Rdata_SS_model_parameters_data.R
+++ R/Rdata_SS_model_parameters_data.R
@@ -10,12 +10,9 @@ rm(list = ls(all.names = TRUE))
 # load packages ----
 
 library(r4ss)
+library(readr)
+
 source(file=file.path(here::here(), "R", "utils", "ss_utils.R"))
-
-# Data ----
-
-
-
 
 # Local declarations ----
 fsep <- .Platform$file.sep
@@ -24,9 +21,52 @@ fsep <- .Platform$file.sep
 dir_model <- file.path(here::here(), "model", fsep = fsep)
 dir_script <- file.path(here::here(), "R", fsep = fsep)
 dir_SS <- file.path(dir_script, "ss", fsep = fsep)
+dir_dat <- file.path(here::here(), "data", "for_ss", fsep = fsep)
 
+# Data ----
+
+list.files(dir_dat, '\\.csv$', full.names = FALSE)
+
+# biological parameters
+fecund <-  read_csv(file.path(dir_dat, "fecundity_2023.csv"))
+growth <-  read_csv(file.path(dir_dat, "growth_2023.csv"))
+allom <-  read_csv(file.path(dir_dat, "wtlen_bysex_2023.csv"))
+# To do - go back and format this correctly
+mature <-  read_csv(file.path(dir_dat, "maturity_alternatives_2023.csv")) %>% 
+  tidyr::pivot_longer(cols = -Method, names_to = 'Param', values_to = 'Value') %>% 
+  dplyr::rename(Version = Method)
+natmat <-  read_csv(file.path(dir_dat, "natmat_2023.csv"))
+
+# survey index and length comp data: alternatives include design vs glmbTMB model
+# based indices and configurations which include and exclude the afsc slope
+# survey
+srv_db_all <-  read_csv(file.path(dir_dat, "survey_db_indices_all_2023.csv"))
+srv_db_noslope <-  read_csv(file.path(dir_dat, "survey_db_indices_noslope_2023.csv"))
+srv_mb_all <-  read_csv(file.path(dir_dat, "survey_mb_indices_all_2023.csv"))
+srv_mb_noslope <-  read_csv(file.path(dir_dat, "survey_mb_indices_noslope_2023.csv"))
+srv_len_all <- read_csv(file.path(dir_dat, "survey_length_comps_all_2023.csv"))
+srv_len_noslope <- read_csv(file.path(dir_dat, "survey_length_comps_noslope_2023.csv"))
+
+# landings catch and length comp data: alternatives include the old 4 fleet
+# structure (NTrawl, NOther, STrawl, SOther) and the new 3 fleet structure
+# (NTrawl, STrawl, Other)
+landings3 <-  read_csv(file.path(dir_dat, "landings_3fleet_2023.csv"))
+landings4 <-  read_csv(file.path(dir_dat, "landings_4fleet_2023.csv"))
+landings_lencomps3 <-  read_csv(file.path(dir_dat, "landings_length_comps_3fleet_2023.csv"))
+landings_lencomps4 <-  read_csv(file.path(dir_dat, "landings_length_comps_4fleet_2023.csv"))
+
+# discard data - To do - go back and format this correctly (use read_csv so i
+# don't have to use [-1] and the same naming conventions
+discard_rates3 <- read_csv(file.path(dir_dat, "discardRates_3Fleets.csv"))[-1]
+discard_rates4 <- read_csv(file.path(dir_dat, "discardRates_4Fleets.csv"))[-1]
+discard_weights3 <- read_csv(file.path(dir_dat, "discardWeights_ss_3Fleets.csv"))[-1]
+discard_weights4 <- read_csv(file.path(dir_dat, "discardWeights_ss_4Fleets.csv"))[-1]
+discard_weights4 <- read_csv(file.path(dir_dat, "discardWeights_ss_4Fleets.csv"))[-1]
+discard_lencomps3 <- read_csv(file.path(dir_dat, "discardLenComp_ss_3Fleets.csv"))[-1]
+discard_lencomps4 <- read_csv(file.path(dir_dat, "discardLenComp_ss_4Fleets.csv"))[-1]
 
 # Stuff needed ----
+
 # Colnames from SS
 lng_par_colnames <- c(
   "LO", "HI", "INIT", "PRIOR", "PR_SD", "PR_type", "PHASE",
@@ -37,7 +77,6 @@ srt_par_colnames <- c("LO", "HI", "INIT", "PRIOR", "PR_SD", "PR_type", "PHASE")
 
 # If we just want the parameter and the value
 ParVal_colnames <- c("Parameter", "Value")
-
 
 # Internal functions
 # Build empty data frame
@@ -124,30 +163,81 @@ SS_Param2023$Comments$Content <- "This is a Rdata to save the parameters and dat
 # MG Parameters ----
 SS_Param2023$MG_params$Content <- "These are the mortality_growth parameters"
 
-# Natural mortality
-rowNam_M <- getName(string = MG_row, pattern = "NatM")
-# SS_Param2023$MG_params$M <- doDat(Data_colnames = lng_par_colnames, RowNam = rowNam_M)
-SS_Param2023$MG_params$M <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_M)
+# Natural mortality ----
 
-# Growth
+rowNam_M <- getName(string = MG_row, pattern = "NatM")
+
+# Base 2023 (Hamel and Cope 2022 method)
+SS_Param2023$MG_params$M$base_2023 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_M)
+SS_Param2023$MG_params$M$base_2023$Value <- c(natmat$initM, 0, 0, 0)
+
+# 2013 sensitivity
+SS_Param2023$MG_params$M$assess_2013 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_M)
+SS_Param2023$MG_params$M$assess_2013$Value <- c(0.0505, 0, 0, 0) 
+
+# Growth ----
 rowNam_G <- getName(string = MG_row, pattern = c("L_at", "VonBert_K", "CV_"))
 # SS_Param2023$MG_params$Growth <- doDat(Data_colnames = lng_par_colnames, RowNam = rowNam_G)
-SS_Param2023$MG_params$Growth <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_G)
 
-# Weight-Length relationship
+unique(growth$Version)
+
+# Base 2023 (Schnute fit to Butler data set, lognormal distribution)
+SS_Param2023$MG_params$Growth$base_2023 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_G)
+SS_Param2023$MG_params$Growth$base_2023$Value <- growth$Value[growth$Version == 'Butler_Base_2023']
+
+# Upper sensitivity
+SS_Param2023$MG_params$Growth$upper_sensitivity <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_G)
+SS_Param2023$MG_params$Growth$upper_sensitivity$Value <- growth$Value[growth$Version == 'Increase_L_at_Amin_and_Amax_25perc']
+
+# Lower sensitivity
+SS_Param2023$MG_params$Growth$lwr_sensitivity <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_G)
+SS_Param2023$MG_params$Growth$lwr_sensitivity$Value <- growth$Value[growth$Version == 'Decrease_L_at_Amin_and_Amax_10perc']
+
+# 2013 assessment sensitivity
+SS_Param2023$MG_params$Growth$assess_2013 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_G)
+SS_Param2023$MG_params$Growth$assess_2013$Value <- growth$Value[growth$Version == '2013_assessment']
+
+# Weight-Length relationship -----
 rowNam_WL <- getName(string = MG_row, pattern = c("Wtlen_"))
 # SS_Param2023$MG_params$WL <- doDat(Data_colnames = lng_par_colnames, RowNam = rowNam_WL)
-SS_Param2023$MG_params$WL <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_WL)
+unique(allom$Version)
 
-# Maturity
+# Base 2023 (updated using WCBTS data)
+SS_Param2023$MG_params$WL$base_2023 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_WL)
+SS_Param2023$MG_params$WL$base_2023$Value <- allom$Value[allom$Version == 'WCBTS_Base_2023']
+
+# 2013 assessment sensitivity
+SS_Param2023$MG_params$WL$assess_2013 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_WL)
+SS_Param2023$MG_params$WL$assess_2013$Value <- allom$Value[allom$Version == '2013_assessment']
+
+# Maturity ----
 rowNam_Mat <- getName(string = MG_row, pattern = c("Mat_", "Mat50"))
 # SS_Param2023$MG_params$Mat <- doDat(Data_colnames = lng_par_colnames, RowNam = rowNam_Mat)
-SS_Param2023$MG_params$Mat <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_Mat)
+unique(mature$Version)
 
-# Fecundity
+# Base 2023 (GLM with quadratics on latitude and depth fit to M Head 2023 data set)
+SS_Param2023$MG_params$Mat$base_2023 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_Mat)
+SS_Param2023$MG_params$Mat$base_2023$Value <- mature$Value[mature$Version == 'Head_2023']
+
+# Pearson and Gunderson 2003 (used in 2013 assessment) - use as sensitivity
+SS_Param2023$MG_params$Mat$assess_2013 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_Mat)
+SS_Param2023$MG_params$Mat$assess_2013$Value <- mature$Value[mature$Version == 'Pearson_and_Gunderson_2003']
+
+# Intermediate sensitivity - average of Head 2023 and Pearson and Gunderson 2003 
+SS_Param2023$MG_params$Mat$interm_sensitivity <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_Mat)
+SS_Param2023$MG_params$Mat$interm_sensitivity$Value <- mature$Value[mature$Version == 'Intermediate_sensitivity']
+
+# Fecundity----
 rowNam_Fec <- getName(string = MG_row, pattern = c("Eggs_"))
 # SS_Param2023$MG_params$Fec <- doDat(Data_colnames = lng_par_colnames, RowNam = rowNam_Fec)
-SS_Param2023$MG_params$Fec <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_Fec)
+unique(fecund)
+
+# Base 2023 - use Cooper 2005 TODO - check scale!
+SS_Param2023$MG_params$Fec$base_2023 <- doDat(Data_colnames = ParVal_colnames, RowNam = rowNam_Fec)
+SS_Param2023$MG_params$Fec$base_2023$Value <- fecund$Value
+
+# TODO - sensitivity to 2013 assessment assumptions spawning output = spawning biomass 
+# not sure how to add this using this structure
 
 # Deviations
 rowNam_Dev <- getName(string = MG_row, pattern = c("Dev"))
@@ -189,41 +279,77 @@ SS_Param2023$init_F$param <- doDat(Data_colnames = ParVal_colnames, RowNam = Ini
 # Catch data ----
 SS_Param2023$Catch$Content <- "These are the catch data"
 Catch_colnames <- c("year", "season", "fleet", "catch", "catch_se")
-SS_Param2023$Catch$data <- doDat(Data_colnames = Catch_colnames, RowNam = NULL)
-# ********************************
+# doDat(Data_colnames = Catch_colnames, RowNam = NULL)
+names(landings3) == Catch_colnames
+names(landings4) == Catch_colnames
+
+SS_Param2023$Catch$data$fleet4 <- landings4
+SS_Param2023$Catch$data$fleet3 <- landings3
 
 # Survey data ----
 SS_Param2023$Indices$Content <- "These are the survey data"
 Srv_colnames <- c("year", "season", "index", "obs", "se_log")
-SS_Param2023$Indices$data <- doDat(Data_colnames = Srv_colnames, RowNam = NULL)
-# ********************************
+# doDat(Data_colnames = Srv_colnames, RowNam = NULL)
+names(srv_db_all) == Srv_colnames
+SS_Param2023$Indices$data$mb_all <- srv_mb_all
+SS_Param2023$Indices$data$mb_noslope <- srv_mb_noslope
+SS_Param2023$Indices$data$db_all <- srv_db_all
+SS_Param2023$Indices$data$db_noslope <- srv_db_noslope
 
 # Discard data ----
 SS_Param2023$discard_fleets$Content <- "These are the discards data"
 Dscd_colnames <- c("year", "season", "fleet", "discard", "std_in")
-SS_Param2023$discard_fleets$data <- doDat(Data_colnames = Dscd_colnames, RowNam = NULL)
-# ********************************
+# doDat(Data_colnames = Dscd_colnames, RowNam = NULL)
+names(discard_rates4) <- Dscd_colnames
+names(discard_rates3) <- Dscd_colnames
+SS_Param2023$discard_fleets$data$fleet4 <- discard_rates4
+SS_Param2023$discard_fleets$data$fleet3 <- discard_rates3
+
+discard_
 
 # Meanbodywt data ----
 SS_Param2023$Meanbodywt$Content <- "These are the Meanbodywt (weight/length) data"
 MeanBody_colnames <- c("year", "season", "fleet", "partition", "type", "obs", "std_in")
-SS_Param2023$Meanbodywt$data <- doDat(Data_colnames = MeanBody_colnames, RowNam = NULL)
-# ********************************
+names(discard_weights4) <- MeanBody_colnames
+names(discard_weights3) <- MeanBody_colnames
+# doDat(Data_colnames = MeanBody_colnames, RowNam = NULL)
+SS_Param2023$Meanbodywt$data$fleet4 <- discard_weights4
+SS_Param2023$Meanbodywt$data$fleet3 <- discard_weights3
 
-#  data ----
+#  fishery lencomps data ----
 SS_Param2023$Fishery_LengthComp$Content <- "These are the fishery length composition data"
 Lcomp_colnames <- c("year","season","fleet","sex","partition","Nsamp", paste0("f", Lbin_vect),paste0("m", Lbin_vect))
-SS_Param2023$Fishery_LengthComp$data <- doDat(Data_colnames = Lcomp_colnames, RowNam = NULL)
-# ********************************
+# doDat(Data_colnames = Lcomp_colnames, RowNam = NULL)
+names(landings_lencomps4) <- Lcomp_colnames
+names(landings_lencomps3) <- Lcomp_colnames
+names(discard_lencomps4) <- Lcomp_colnames
+names(discard_lencomps3) <- Lcomp_colnames
+fshcomps4 <- rbind(landings_lencomps4, discard_lencomps4)
+fshcomps3 <- rbind(landings_lencomps3, discard_lencomps3)
 
-#  data ----
+SS_Param2023$Fishery_LengthComp$data$fleet4 <- fshcomps4
+SS_Param2023$Fishery_LengthComp$data$fleet3 <- fshcomps3
+  
+#  survey length comps ----
 SS_Param2023$Survey_LengthComp$Content <- "These are the survey length composition data"
 Lcomp_colnames <- c("year","season","fleet","sex","partition","Nsamp", paste0("f", Lbin_vect),paste0("m", Lbin_vect))
-SS_Param2023$Survey_LengthComp$data <- doDat(Data_colnames = Lcomp_colnames, RowNam = NULL)
+names(srv_len_all) <- Lcomp_colnames
+names(srv_len_noslope) <- Lcomp_colnames
+# doDat(Data_colnames = Lcomp_colnames, RowNam = NULL)
+SS_Param2023$Survey_LengthComp$data$all <- srv_len_all
+SS_Param2023$Survey_LengthComp$data$noslope <- srv_len_noslope
 
-# ********************************
+#  survey length comps ----
+SS_Param2023$Survey_LengthComp$Content <- "These are the survey length composition data"
+Lcomp_colnames <- c("year","season","fleet","sex","partition","Nsamp", paste0("f", Lbin_vect),paste0("m", Lbin_vect))
+names(srv_len_all) <- Lcomp_colnames
+names(srv_len_noslope) <- Lcomp_colnames
+# doDat(Data_colnames = Lcomp_colnames, RowNam = NULL)
+SS_Param2023$Survey_LengthComp$data$all <- srv_len_all
+SS_Param2023$Survey_LengthComp$data$noslope <- srv_len_noslope
+
 
 # ===========================================================================
 
 save(SS_Param2023,
-     file = file.path(dir_SS, "SS_2023_Data_Parameters.RData", fsep = fsep))
+     file = file.path(dir_SS, "SST_SS_2023_Data_Parameters.RData", fsep = fsep))
