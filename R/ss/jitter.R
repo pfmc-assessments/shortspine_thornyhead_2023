b6921f697dabf1a800e0447090641cbddc2fa1ff
Joshua Zahner <joshuazahner@gmail.com>
2023-06-19 17:02:01 -0700

    jitter for updated base model

--- R/ss/jitter.R
+++ R/ss/jitter.R
@@ -2,7 +2,7 @@ library(here)
 library(r4ss)
 source(file=file.path(here::here(), "R", "utils", "ss_utils.R"))
 
-model_settings <- get_settings(settings = list(base_name = '1_23.model.francis_2',
+model_settings <- get_settings(settings = list(base_name = '1_23.base.official',
                                                run = "jitter",
                                                init_values_src = 0,
                                                usepar=FALSE,
@@ -15,13 +15,21 @@ model_settings <- get_settings(settings = list(base_name = '1_23.model.francis_2
 
 
 model_settings$exe <- 'ss_osx'
-file.copy(here::here("model/ss_executables/SS_V3_30_21/ss_osx"), here::here("model/Sensitivity_Anal/5.8_Francis_Reweighting_2/1_23.model.francis_2"))
-run_diagnostics_MacOS(mydir = here::here("model/Sensitivity_Anal/5.8_Francis_Reweighting_2/"), 
+file.copy(here::here("model/ss_executables/SS_V3_30_21/ss_osx"), here::here("model/Sensitivity_Anal/Base_Model/5.23_Official_Base/1_23.base.official/"))
+run_diagnostics_MacOS(mydir = here::here("model/Sensitivity_Anal/Base_Model/5.23_Official_Base/"), 
                       model_settings = model_settings)
 
 #### Get starting values
-i=1 # Check CSV to find a model with optimal likelihood
-file = file.path(here::here("model/Sensitivity_Anal/5.8_Francis_Reweighting_2/1_23.model.francis_2_jitter_0.1"), paste0("ParmTrace",i,".sso"))
+i=3 # Check CSV to find a model with optimal likelihood
+file = file.path(here::here("model/Sensitivity_Anal/Base_Model/5.23_Official_Base/1_23.base.official_jitter_0.1/"), paste0("ParmTrace",i,".sso"))
 pars.i <- read.table(file, header = TRUE, nrows = 1, check.names = FALSE, sep="\t")
 
+jitter.dir <- file.path(here::here(), "model/Sensitivity_Anal/Base_Model/jitter/")
+if(file.exists(jitter.dir)){
+  unlink(jitter.dir, recursive = TRUE)
+}
 
+file.rename(
+  from=file.path(here::here(), "model/Sensitivity_Anal/Base_Model/5.23_Official_Base/1_23.base.official_jitter_0.1/"),
+  to=jitter.dir
+)
